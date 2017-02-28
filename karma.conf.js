7a7f9f12d97d1845b57813d14c14d32ba6e6721e
Uncharted <alexandre.mai@gmail.com>
2017-02-28 18:44:31 +0100

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function (config) {
             // and require all tests in there
             'test/test.js': ['webpack'],
             //'src/**/*.js': ['coverage'],
-            'src/**/*.js'
+            'src/**/*.js': ['webpack']
         },
 
         reporters: ['spec', 'junit', 'coverage'],
